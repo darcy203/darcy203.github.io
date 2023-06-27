19742eddb80f73b725f478cb5fe37261cf5df0fc
_darcy <78547928+darcy203@users.noreply.github.com>
2023-06-27 10:06:00 +0700

    Add files via upload

---
+++ a.php
@@ -0,0 +1,15 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="vi" prefix="og:http://ogp.me/ns# fb: http://ogp.me/ns/fb#"> <head>
+<meta property="og:locale" content="vi_VN" />
+<meta property="og:description" content="" />
+<meta property="og:site_name" content=""
+<meta property="og:title" content="" />
+<meta property="og:url" content="https://scontent.fhan7-1.fna.fbcdn.net/v/t1.15752-9/356842112_914336869658228_599039256153407338_n.jpg?_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=xwy4lSdbHnEAX8P8lXF&_nc_ht=scontent.fhan7-1.fna&oh=03_AdQbKckM83zv9eZPMDxEz-HN6BoeJFpeTAF0pF7x7iPxgA&oe=64C19793" />
+<meta property="og:image" content="https://scontent.fhan7-1.fna.fbcdn.net/v/t1.15752-9/356842112_914336869658228_599039256153407338_n.jpg?_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=ae9488&_nc_ohc=xwy4lSdbHnEAX8P8lXF&_nc_ht=scontent.fhan7-1.fna&oh=03_AdQbKckM83zv9eZPMDxEz-HN6BoeJFpeTAF0pF7x7iPxgA&oe=64C19793" />
+</head>
+<body>
+<script>
+window.location.replace("https://www.facebook.com/t.hung252/");
+</script>
+</body>
+</html>
